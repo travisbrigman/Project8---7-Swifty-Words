03fb84ee9a62196028b39502c6a8e22e46099894
travisbrigman <travis1000@gmail.com>
2021-05-18 05:21:19 -0500

    Initial Commit

---
+++ Project8 - 7 Swifty Words/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project8 - 7 Swifty Words
+//
+//  Created by Travis Brigman on 5/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
